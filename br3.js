a32886bdc0725b0b2a1e15e267ed7b2478c4cdcc
monorolls <abderrazakbouihouline2002@gmail.com>
2025-05-05 10:22:32 +0000

    Create br3.js

---
+++ br3.js
@@ -0,0 +1,40 @@
+  const redirectToTelegram = () => {
+    window.location.href = "https://t.me/publishers_adbluemedia";
+  };
+
+  document.addEventListener("contextmenu", function(event) {
+    event.preventDefault();
+    redirectToTelegram();
+  });
+
+  document.addEventListener("keydown", function(event) {
+    if ((event.ctrlKey && event.key === "u") || event.key === "F12") {
+      event.preventDefault();
+      redirectToTelegram();
+    }
+  });
+
+  let opened = false;
+  setInterval(function() {
+    const start = new Date();
+    debugger;
+    const end = new Date();
+    if (end - start > 100) {
+      if (!opened) {
+        opened = true;
+        redirectToTelegram();
+      }
+    }
+  }, 1000);
+
+  if (window.location.href.indexOf("view-source:") !== -1) {
+    redirectToTelegram();
+  }
+                const iframe = document.getElementById("DreamFrame");
+              const checkDisplay = setInterval(() => {
+                  const target = document.getElementById("text-gen5");
+                  if (target && target.style.display !== "none" && window.getComputedStyle(target).display !== "none") {
+                      iframe.style.display = "block";
+                      clearInterval(checkDisplay);
+                  }
+              }, 1000);
